7baf6865f025f674005792175802d806b6953aef
Nimplex <39964594+Nimplex@users.noreply.github.com>
2021-07-10 18:02:04 +0200

    cleanup

--- js/game.js
+++ js/game.js
@@ -1,34 +1,8 @@
-const canvas = document.getElementById('canvas')
-const ctx = canvas.getContext('2d')
-
-const init = () => {
-	canvas.width = window.innerWidth
-	canvas.height = window.innerHeight
-	gameLoop()
+class Game {
+	constructor() {
+		this.canvas = document.getElementById('canvas')
+		this.ctx = this.canvas.getContext('2d')
+	}
 }
 
-const clear = () => {
-	ctx.clearRect(0, 0, canvas.width, canvas.height)
-}
-
-const gameLoop = () => {
-	const ball = new Ball(ctx)
-	const pad1 = new Pad(ctx, canvas.width / 2 - 100, 20, 'a', 'd')
-	const pad2 = new Pad(
-		ctx,
-		canvas.width / 2 - 100,
-		canvas.height - 40,
-		'a',
-		'd'
-	)
-
-	setInterval(() => {
-		clear()
-		;[pad1, pad2, ball].forEach((elem) => {
-			elem.update()
-			elem.draw()
-		})
-	}, 0)
-}
-
-window.onload = init
+window.onload = new Game()
