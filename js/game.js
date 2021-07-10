dd919fd3a955702379f558b4c20366a27f043131
Nimplex <39964594+Nimplex@users.noreply.github.com>
2021-07-10 18:28:32 +0200

    draw pads and balls

--- js/game.js
+++ js/game.js
@@ -1,3 +1,44 @@
+class Ball {
+	constructor(ctx, canvas) {
+		this.ctx = ctx
+		this.canvas = canvas
+		this.size = 20
+		this.x = this.canvas.width / 2 - this.size / 2
+		this.y = this.canvas.height / 2 - this.size / 2
+	}
+	update() {
+
+	}
+	draw() {
+		this.ctx.fillRect(this.x, this.y, this.size, this.size)
+	}
+}
+
+class Pad {
+	constructor(ctx, canvas, pos) {
+		this.ctx = ctx
+		this.canvas = canvas
+		this.width = 80
+		this.height = 20
+		switch(pos) {
+			case 'up':
+				this.x = this.canvas.width / 2 - this.width / 2
+				this.y = this.height * 2
+				break;
+			case 'down':
+				this.x = this.canvas.width / 2 - this.width / 2
+				this.y = this.canvas.height - this.height * 2
+				break;
+		}
+	}
+	update() {
+
+	}
+	draw() {
+		this.ctx.fillRect(this.x, this.y, this.width, this.height)
+	}
+}
+
 class Game {
 	constructor() {
 		this.canvas = document.getElementById('canvas')
@@ -5,6 +46,10 @@ class Game {
 
 		this.canvas.width  = document.documentElement.clientWidth
 		this.canvas.height = document.documentElement.clientHeight
+
+		this.ball = new Ball(this.ctx, this.canvas)
+		this.player1 = new Pad(this.ctx, this.canvas, 'up')
+		this.player2 = new Pad(this.ctx, this.canvas, 'down')
 
 		setInterval(() => { this.update(); this.draw() }, 0)
 	}
@@ -18,6 +63,10 @@ class Game {
 		this.clear()
 		this.ctx.fillStyle = '#DAF7A6'
 		this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height)
+		this.ctx.fillStyle = '#FFFFFF'
+		this.ball.draw()
+		this.player1.draw()
+		this.player2.draw()
 	}
 }
 
