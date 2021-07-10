e1adbd3ae62b69b091f5e307932a82db8f402ef5
Nimplex <39964594+Nimplex@users.noreply.github.com>
2021-07-10 18:09:56 +0200

    basic game loop

--- js/game.js
+++ js/game.js
@@ -2,6 +2,22 @@ class Game {
 	constructor() {
 		this.canvas = document.getElementById('canvas')
 		this.ctx = this.canvas.getContext('2d')
+
+		this.canvas.width  = document.documentElement.clientWidth
+		this.canvas.height = document.documentElement.clientHeight
+
+		setInterval(() => { this.update(); this.draw() }, 0)
+	}
+	update() {
+
+	}
+	clear() {
+		this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
+	}
+	draw() {
+		this.clear()
+		this.ctx.fillStyle = '#DAF7A6'
+		this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height)
 	}
 }
 
